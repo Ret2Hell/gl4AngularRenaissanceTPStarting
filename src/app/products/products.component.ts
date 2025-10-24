dee173345083606d3a9d2263816fdae794bf038d
Ret2Hell <tayeb1920@gmail.com>
2025-10-24 13:23:34 +0100

    chore: migrate routing

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,25 +1,14 @@
-import { Component } from "@angular/core";
-import {
-  BehaviorSubject,
-  Observable,
-  concatMap,
-  map,
-  takeWhile,
-  scan,
-} from "rxjs";
-import { Product } from "./dto/product.dto";
-import { ProductService } from "./services/product.service";
-import { Settings } from "./dto/product-settings.dto";
-import { AsyncPipe } from "@angular/common";
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './dto/product.dto';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
-    selector: "app-products",
-    templateUrl: "./products.component.html",
-    styleUrls: ["./products.component.css"],
-    standalone: true,
-    imports: [
-    AsyncPipe
-],
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
+  standalone: true,
+  imports: [AsyncPipe],
 })
 export class ProductsComponent {
   /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
